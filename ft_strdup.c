a33bdc1e6e61fa785819409f6c8c04f376a7d2ea
cvsgbhn <54961302+cvsgbhn@users.noreply.github.com>
2019-09-16 15:44:31 +0300

    Add files via upload

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,37 +6,13 @@
 /*   By: vdanilo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/08 15:04:30 by vdanilo           #+#    #+#             */
-/*   Updated: 2019/09/09 14:38:41 by vdanilo          ###   ########.fr       */
+/*   Updated: 2019/09/16 15:26:10 by vdanilo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int		ft_strlen(char *str)
-{
-	int counter;
-
-	counter = 0;
-	while (*str)
-	{
-		counter++;
-		str++;
-	}
-	return (counter);
-}
-
-char	*ft_strcpy(char *string1, char *string2)
-{
-	while (*string1 && *string2)
-	{
-		*string2 = *string1;
-		string1++;
-		string2++;
-	}
-	return (string2);
-}
-
-char	*ft_strdup(char *src)
+char	*ft_strdup(const char *src)
 {
 	char *copy;
 
