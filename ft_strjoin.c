1d31d7c29739574b5bff07edf02697d38c971f0f
cvsgbhn <kbh123456g@gmail.com>
2019-09-20 01:17:13 +0300

    wow it even works

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -12,5 +12,17 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	
-}
+	size_t len_s1;
+	size_t len_s2;
+	int sum_len;
+	char *concatenated;
+
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	concatenated = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+	ft_strcat(s1, s2);
+	ft_strcpy(concatenated, s1);
+	sum_len = len_s1 + len_s2;
+	concatenated[sum_len] = '\0';
+	return (concatenated);
+	}
