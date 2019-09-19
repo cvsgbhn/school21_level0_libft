ecdc85af6b0bddc34e9029385b832184592f7505
cvsgbhn <kbh123456g@gmail.com>
2019-09-20 02:23:14 +0300

    main part is accomplished

--- ft_libft.h
+++ ft_libft.h
@@ -53,12 +53,12 @@ void    ft_strdel(char **as);
 void    ft_strclr(char *s);
 void    ft_striter(char *s, void (*f)(char *));
 void    ft_striteri(char *s, void (*f)(unsigned int, char *));
-//ft_strmap
-//ft_strmapi
+char	*ft_strmap(char const *s, char (*f)(char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int     ft_strequ(char const *s1, char const *s2);
 int     ft_strnequ(char const *s1, char const *s2, size_t n);
 char    *ft_strsub(char const *s, unsigned int start, size_t len);
-//ft_strjoin
+char	*ft_strjoin(char const *s1, char const *s2);
 char    *ft_strtrim(char const *s);
 char    **ft_strsplit(char const *s, char c);
 char    *ft_itoa(int n);
